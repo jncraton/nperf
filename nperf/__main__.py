19f4229d4388b62d3c0edca4b27165a6274b6627
Jon Craton <jncraton@gmail.com>
2020-06-27 16:40:51 -0400

    Add latency info

--- nperf/__main__.py
+++ nperf/__main__.py
@@ -1,4 +1,7 @@
 from nperf.web import get_down_speed
+from nperf.shell import check_ping, get_default_gateway
 
 def main():
- print(get_down_speed())
+ print(f'Local latency: {check_ping(get_default_gateway()):.02f}ms')
+ print(f'Internet latency: {check_ping():.02f}ms')
+ print(f'Internet bandwidth {get_down_speed():.02f}mbps')
