2da17d3ebcf8be7cf94c7a83f0e8ef7c5c834880
Jon Craton <jncraton@gmail.com>
2020-06-27 10:59:10 -0400

    Add ping a default gateway tools

--- nperf/shell.py
+++ nperf/shell.py
@@ -1,11 +1,30 @@
-from subprocess import call
+from subprocess import check_output
 import re
 
-def ping(host):
-    """ Pings a host
-
-    >>> ping("1.1.1.1")
-    1.0
+def get_default_gateway():
+    """ Returns the current default gateway
+    
+    >>> get_default_gateway() # doctest: +SKIP
+    '192.168.1.1'
     """
 
-    call(['ping', '-c', 1, '-W', 1, host])
+    output = check_output(['ip','route','show']).decode('utf8')
+    return re.search('default via ([\d\.]+)', output).group(1)
+
+def check_ping(host, timeout=1):
+    """ Checks the ICMP roundtrip ping time to a hosts
+
+    Returns ping time in milliseconds
+
+    >>> check_ping("1.1.1.1") # doctest: +SKIP
+    4.5
+
+    >>> check_ping("1.1.1.1") > 0
+    True
+
+    >>> check_ping("1.1.1.1") < 100
+    True
+    """
+
+    output = check_output(['ping', '-c', '1', '-W', str(timeout), host]).decode('utf8')
+    return float(re.search('time\=([\d\.]+)', output).group(1))
